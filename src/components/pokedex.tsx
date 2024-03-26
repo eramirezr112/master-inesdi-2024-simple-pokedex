297b2568787e54cebb9399a884fcb18842db20ab
Esteban Ramirez <esteban.ramirez@5e.cr>
2024-03-26 04:40:16 -0600

    Solucion de Ejercicio #4

--- src/components/pokedex.tsx
+++ src/components/pokedex.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import c from "classnames";
 import { useTheme } from "contexts/use-theme";
 import { usePokemon, usePokemonList, useTextTransition } from "hooks";
@@ -13,7 +14,9 @@ export function Pokedex() {
   const { ready, resetTransition } = useTextTransition();
   const { pokemonList } = usePokemonList();
   const [i, setI] = useState(0);
-  const { pokemon: selectedPokemon } = usePokemon(pokemonList[i]);
+  const [favorites, setFavorites] = useState([]);
+  const [displayMsj, setDisplayMsj] = useState(false);
+  const { pokemon: selectedPokemon, debilidades } = usePokemon(pokemonList[i]);
   const { pokemon: nextPokemon } = usePokemon(pokemonList[i + 1]);
 
   const prev = () => {
@@ -32,60 +35,160 @@ export function Pokedex() {
     setI((i) => i + 1);
   };
 
+  const addToFavorites = (event: any) => {
+    event.preventDefault();
+    if (favorites.length <= 5) {
+      const newFavorites = [...favorites];
+      newFavorites.push(selectedPokemon);
+      setFavorites(newFavorites);
+      console.log(newFavorites);
+    } else {
+      setDisplayMsj(true);
+    }
+  };
+
   return (
-    <div className={c("pokedex", `pokedex-${theme}`)}>
-      <div className="panel left-panel">
-        <div className="screen main-screen">
-          {selectedPokemon && (
-            <img
+    <div>
+      <div className={c("pokedex", `pokedex-${theme}`)}>
+        <div className="panel left-panel">
+          <div className="pokemon-info-container">
+            <div
+              style={{
+                padding: 20,
+                background: "#fff",
+                borderRadius: 10,
+                margin: 2,
+                marginRight: 8,
+
+                border: "solid 4px #000",
+              }}
+            >
+              <h4 style={{ margin: 0 }}>WEAKNESS:</h4>
+              <ul style={{ margin: 0, padding: 0, listStyleType: "none" }}>
+                {debilidades.map((debilidad: any, i: number) => {
+                  return <li key={`Debilidad-` + i}>{debilidad}</li>;
+                })}
+              </ul>
+            </div>
+            <div className="screen main-screen">
+              {selectedPokemon && (
+                <img
+                  className={c(
+                    "sprite",
+                    "obfuscated",
+                    ready && "ready",
+                    ready && `ready--${randomMode()}`
+                  )}
+                  src={selectedPokemon.sprites.front_default}
+                  alt={selectedPokemon.name}
+                />
+              )}
+            </div>
+            <div
+              style={{
+                padding: 20,
+                background: "#fff",
+                borderRadius: 10,
+                margin: 2,
+                marginLeft: 8,
+                border: "solid 4px #000",
+              }}
+            >
+              <h4 style={{ margin: 0 }}>TYPE</h4>
+              <ul style={{ margin: 0, padding: 0, listStyleType: "none" }}>
+                {selectedPokemon?.types.map((slot, i) => {
+                  return <li key={`type-` + i}>{slot.type.name}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="screen name-display">
+            <div
               className={c(
-                "sprite",
+                "name",
                 "obfuscated",
                 ready && "ready",
                 ready && `ready--${randomMode()}`
               )}
-              src={selectedPokemon.sprites.front_default}
-              alt={selectedPokemon.name}
-            />
-          )}
-        </div>
-        <div className="screen name-display">
-          <div
-            className={c(
-              "name",
-              "obfuscated",
-              ready && "ready",
-              ready && `ready--${randomMode()}`
-            )}
+            >
+              {selectedPokemon?.name}
+            </div>
+          </div>
+          <button
+            type="button"
+            style={{
+              fontSize: 25,
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+            }}
+            onClick={addToFavorites}
           >
-            {selectedPokemon?.name}
+            ⭐
+          </button>
+          <div
+            style={{
+              background: "#fff",
+              color: "red",
+              border: "solid 3px #000",
+              padding: 10,
+              display: displayMsj ? "block" : "none",
+            }}
+          >
+            All Your Team is completed
+          </div>
+        </div>
+        <div className="panel right-panel">
+          <div className="controls leds">
+            <LedDisplay color="blue" />
+            <LedDisplay color="red" />
+            <LedDisplay color="yellow" />
+          </div>
+          <div className="screen second-screen">
+            {nextPokemon && (
+              <img
+                className={c(
+                  "sprite",
+                  "obfuscated",
+                  ready && "ready",
+                  ready && `ready--${randomMode()}`
+                )}
+                src={nextPokemon.sprites.front_default}
+                alt={nextPokemon.name}
+              />
+            )}
+          </div>
+          <div className="controls">
+            <Button label="prev" onClick={prev} />
+            <Button label="next" onClick={next} />
           </div>
         </div>
       </div>
-      <div className="panel right-panel">
-        <div className="controls leds">
-          <LedDisplay color="blue" />
-          <LedDisplay color="red" />
-          <LedDisplay color="yellow" />
-        </div>
-        <div className="screen second-screen">
-          {nextPokemon && (
-            <img
-              className={c(
-                "sprite",
-                "obfuscated",
-                ready && "ready",
-                ready && `ready--${randomMode()}`
-              )}
-              src={nextPokemon.sprites.front_default}
-              alt={nextPokemon.name}
-            />
-          )}
-        </div>
-        <div className="controls">
-          <Button label="prev" onClick={prev} />
-          <Button label="next" onClick={next} />
-        </div>
+      <div className={c("myTeam", `myTeam-gray`)}>
+        <h2 style={{ margin: 0 }}>
+          MY TEAM
+          <span style={{ fontSize: 14 }}>
+            ({favorites.length} Pokemons in your team)
+          </span>
+        </h2>
+        <ul className="teamList">
+          {favorites.map((f, i) => {
+            return (
+              <li key={`favorite-${i}`}>
+                <img
+                  className={c(
+                    "sprite",
+                    "obfuscated",
+                    ready && "ready",
+                    ready && `ready--${randomMode()}`
+                  )}
+                  src={f.sprites.front_default}
+                  alt={f.name}
+                />
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
--- src/hooks/use-pokemon.ts
+++ src/hooks/use-pokemon.ts
@@ -6,6 +6,8 @@ const cache = new Map<string, Pokemon>();
 export function usePokemon(pokemonUri: PokemonUri) {
   const [isLoading, setIsLoading] = useState(true);
   const [pokemon, setPokemon] = useState<Pokemon>();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [debilidades, setDebilidades]= useState<any>([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -14,6 +16,24 @@ export function usePokemon(pokemonUri: PokemonUri) {
         const data = await response.json();
         cache.set(pokemonUri.url, data);
         setPokemon(data);
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const types = data.types.map((type: { type: { name: any; }; }) => type.type.name);        
+        
+        let weaknesses = [];
+        for (const type of types) {          
+            const typeResponse = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+            const typeData = await typeResponse.json();
+            console.log(typeData);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const doubleDamageTo = typeData.damage_relations.double_damage_to.map((damage: { name: any; }) => damage.name);
+            weaknesses = weaknesses.concat(doubleDamageTo);
+        }
+        
+        weaknesses = [...new Set(weaknesses)];        
+
+        setDebilidades(weaknesses);
+
       } catch (error) {
         console.error("Error fetching Pokemon:", error);
       } finally {
@@ -31,5 +51,5 @@ export function usePokemon(pokemonUri: PokemonUri) {
     }
   }, [pokemonUri]);
 
-  return { pokemon, isLoading };
+  return { pokemon, isLoading, debilidades};
 }
